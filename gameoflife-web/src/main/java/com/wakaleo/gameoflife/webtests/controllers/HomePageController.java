1d6e258babe5076f99f42d196a7929136f5a9d77
priyanka kapur <priyanka.potwar@gmail.com>
2012-10-11 12:45:31 +0530

    priyanka - revert changes

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
@@ -14,7 +14,7 @@ public class HomePageController {
     // FIXME: do something nicer for the home page
     @RequestMapping("/")
     public ModelAndView index() {
-        System.out.println("GameofLife Change to trigger build AGAIN and AGAIN   ");
+        System.out.println("GameofLife Change to trigger build AGAIN and AGAIN 3   ");
         return new ModelAndView("home");
 
     }
